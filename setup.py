653f0c8f82bab751a0141827749d2a13a0e3f6ef
JohnyTheCarrot <tuurmartens4@gmail.com>
2019-12-08 19:55:00 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Snowflake',
+      version='1.0',
+      description='Snowflake generator.',
+      author='JohnyTheCarrot',
+      author_email='johnythecarrot@gmail.com',
+      url='https://github.com/JohnyTheCarrot/snowflake',
+      packages=['snowflake']
+)
